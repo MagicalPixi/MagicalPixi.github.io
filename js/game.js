1a3b6ffe21d2c199ee3fed58ca43880cdb77fdd2
zhou-yg <zhou.y.g890@gmail.com>
2016-04-29 00:16:16 +0800

    dragon Jump

--- js/game.js
+++ js/game.js
@@ -4,24 +4,75 @@
 var pixiLib = require('pixi-lib');
 var PIXI = require('PIXI');
 
+
 var gameContainer = document.getElementById('playContainer');
 
+window.maxW = 800;
+window.maxH = 400;
+
 var render = pixiLib.createRender(gameContainer,{
-  w:800,
-  h:600,
+  w:maxW,
+  h:maxH,
 });
 
 var loader = pixiLib.createLoader({
   publicPath:'/sprites/'
 });
 
+window.onresize = function(){
+  requestAnimationFrame(function () {
+
+
+  });
+};
+
+function createRect(){
+
+  var graphics = new PIXI.Graphics();
+
+  var w = 25 + Math.random() * 50;
+  var h = 50 + Math.random() * 100;
+
+  graphics.lineStyle(2, 0x000000, 1);
+  graphics.beginFill(0x000000, 1);
+  graphics.drawRect(maxW, maxH-h, w, h, 0);
+  graphics.endFill();
+
+  var borderW = -(maxW + w);
+
+  graphics.render = function () {
+    this.x -= 10;
+    if(this.x < borderW && this.parent){
+      this.parent.removeChild(this);
+      this.destroy();
+    }
+  };
+
+  return graphics;
+}
+
 loader.add(['dragon']).load(function () {
 
   var stage = new PIXI.Container();
 
   var dragon = require('../sprites/dragon');
-
   dragon.play();
+
+
+  document.addEventListener('keydown', function (e) {
+    if(e.keyCode === 38){
+      dragon.jump();
+    }
+  });
+
+  document.getElementById('playContainer').onclick = function(){
+    dragon.jump();
+  };
+
+  setInterval(function () {
+    var rect = createRect();
+    stage.addChild(rect);
+  },1000);
 
   stage.addChild(dragon);
 
--- sprites/dragon/index.js
+++ sprites/dragon/index.js
@@ -4,9 +4,42 @@ var mySprite = mySpriteFn();
 mySprite.scale.x = 0.1;
 mySprite.scale.y = 0.1;
 mySprite.anchor.y = 1;
-mySprite.y = 600;
+mySprite.x = maxW/5;
+mySprite.y = maxH;
+
+function createJumpFn(){
+
+  var st = Date.now();
+
+  var a = -2;
+
+  return function(){
+    var t = (Date.now()  - st )/20;
+
+    return 30*t + 1/2*Math.pow(t,2)*a;
+  }
+}
+
+mySprite.getJumpHeight = function(){
+  return 0;
+};
 
 mySprite.render = function () {
+  if(this.jumping){
+    var j =  this.getJumpHeight();
+
+    this.y = maxH - j;
+
+    if(this.y > maxH){
+      this.y = maxH;
+      this.jumping = false;
+    }
+  }
+};
+
+mySprite.jump = function () {
+  this.jumping = true;
+  this.getJumpHeight = createJumpFn();
 };
 
 module.exports = mySprite;
