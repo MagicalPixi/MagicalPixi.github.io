17b9a0b5d4903ba8f12ec03191fac310011d4f02
zhou-yg <zhou.y.g890@gmail.com>
2016-06-30 23:15:20 +0800

    加了个关于我们。

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
+require('../css/nav.less')
+require('../css/about-us.less')
 require('../css/index.less')
 require('../css/index.pc.less')
 
 var pixiLib = require('pixi-lib')
+
+var nav = document.querySelector('.top nav')
+
+var ulList = nav.querySelector('ul')
+
+nav.addEventListener('click',function () {
+
+  if(ulList.classList.contains('show')){
+    ulList.classList.remove('show')
+  }else{
+    ulList.classList.add('show')
+  }
+
+
+
+})
