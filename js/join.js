9d3c1c362bbb1ad467ebe3212738021bd909cac4
zhou-yg <zhou.y.g890@gmail.com>
2016-07-03 16:02:23 +0800

    fix style

--- js/join.js
+++ js/join.js
@@ -2,6 +2,7 @@ require('weui/dist/style/weui.min.css')
 require('../css/nav.less')
 require('../css/join.less')
 require('../css/join.pc.less')
+
 var nav = document.querySelector('.top nav')
 var ulList = nav.querySelector('ul')
 nav.addEventListener('click',function () {
