dd06799555f79803f3f52d7652791b40da57324c
zhouyg <yunge@taovip.com>
2016-07-19 14:24:32 +0800

    加入css前缀

--- webpack.config.pro.js
+++ webpack.config.pro.js
@@ -4,19 +4,24 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var basic = require('./webpack.config')
 
+var precss = require('precss')
+var autoprefixer = require('autoprefixer')
+
 basic = Object.assign(basic, {
 
 
   module: {
     loaders: [{
       test: /\.less$/,
-      loader: ExtractTextPlugin.extract("style", "css!less"),
+      loader: ExtractTextPlugin.extract("style", "css!postcss!less"),
     },{
       test: /\.css$/,
       loaders: ['style', 'css']
     }]
   },
-
+  postcss: function () {
+    return [precss, autoprefixer];
+  },
   plugins: [
     new ExtractTextPlugin("[name].css")
   ]
